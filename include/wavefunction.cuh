e80b1b767442711a84a893dc42985f0d5ee1b118
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2022-02-07 14:07:20 +0000

    Added initial wavefunction class

---
+++ include/wavefunction.cuh
@@ -0,0 +1,22 @@
+//
+// Created by mattw on 07/02/2022.
+//
+
+#ifndef BECUDA_WAVEFUNCTION_H
+#define BECUDA_WAVEFUNCTION_H
+
+#include "cufft.h"
+
+// Abstract base class
+class Wavefunction
+{
+
+};
+
+class Wavefunction2D : Wavefunction
+{
+private:
+
+};
+
+#endif //BECUDA_WAVEFUNCTION_H
---
+++ src/wavefunction.cu
@@ -0,0 +1,5 @@
+//
+// Created by mattw on 07/02/2022.
+//
+
+#include "../include/wavefunction.cuh"
