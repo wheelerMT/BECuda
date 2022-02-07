5c6d39c9908eaf9b5afdc80c668fa8ae021218d6
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2022-02-07 15:30:03 +0000

    Added constructor, functions for initial state and FFTs

--- include/wavefunction.cuh
+++ include/wavefunction.cuh
@@ -5,7 +5,10 @@
 #ifndef BECUDA_WAVEFUNCTION_H
 #define BECUDA_WAVEFUNCTION_H
 
-#include "cufft.h"
+#include <cufft.h>
+#include <string>
+#include <iostream>
+#include "grid.cuh"
 
 // Abstract base class
 class Wavefunction
@@ -16,6 +19,30 @@ class Wavefunction
 class Wavefunction2D : Wavefunction
 {
 private:
+    // FFT plans for each wavefunction component
+    cufftHandle m_planPlus{}, m_planZero{}, m_planMinus{};
+
+public:
+    // Constructor
+    explicit Wavefunction2D(Grid2D &grid);
+
+    // Destructor
+    ~Wavefunction2D();
+
+    // FFT-related functions
+    void generateFFTPlans();
+    void executeFFT();
+    void executeIFFT();
+
+    // Initial state functions
+    void setInitialState(const std::string &gsPhase);
+
+    // Reference to grid object
+    Grid2D &grid;
+
+    // Wavefunction components
+    cufftComplex *plus{}, *zero{}, *minus{};
+    cufftComplex *plus_k{}, *zero_k{}, *minus_k{};  // K-space versions
 
 };
 
--- src/wavefunction.cu
+++ src/wavefunction.cu
@@ -2,4 +2,61 @@
 // Created by mattw on 07/02/2022.
 //
 
-#include "../include/wavefunction.cuh"
+#include "wavefunction.cuh"
+
+
+Wavefunction2D::Wavefunction2D(Grid2D &grid) : grid{grid}
+{
+    // Allocate arrays
+    plus = new cufftComplex[grid.nx * grid.ny];
+    zero = new cufftComplex[grid.nx * grid.ny];
+    minus = new cufftComplex[grid.nx * grid.ny];
+    plus_k = new cufftComplex[grid.nx * grid.ny];
+    zero_k = new cufftComplex[grid.nx * grid.ny];
+    minus_k = new cufftComplex[grid.nx * grid.ny];
+
+    // Initialise FFT plans
+    generateFFTPlans();
+}
+
+Wavefunction2D::~Wavefunction2D()
+{
+    // This needs to call appropriate functions
+    // to de-allocate arrays on device memory
+}
+
+void Wavefunction2D::generateFFTPlans()
+{
+    // Generate CUDA FFT plans for each component check for errors
+    if (cufftPlan2d(&m_planPlus, grid.nx, grid.ny, CUFFT_C2C) != CUFFT_SUCCESS)
+    {
+        std::cerr << "CUFFT error: Plan creation failed...\n";
+        return;
+    };
+    if (cufftPlan2d(&m_planZero, grid.nx, grid.ny, CUFFT_C2C) != CUFFT_SUCCESS)
+    {
+        std::cerr << "CUFFT error: Plan creation failed...\n";
+        return;
+    };
+    if (cufftPlan2d(&m_planMinus, grid.nx, grid.ny, CUFFT_C2C) != CUFFT_SUCCESS)
+    {
+        std::cerr << "CUFFT error: Plan creation failed...\n";
+        return;
+    };
+}
+
+void Wavefunction2D::setInitialState(const std::string &gsPhase)
+{
+    if (gsPhase == "polar")
+    {
+        for (int i = 0; i < grid.nx; ++i)
+        {
+            for (int j = 0; j < grid.ny; ++j)
+            {
+                plus[j + i * grid.nx] = {0., 0.};
+                zero[j + i * grid.nx] = {1., 0.};
+                minus[j + i * grid.nx] = {0., 0.};
+            }
+        }
+    }
+}
