21c1b179fe4ec0bfb2591c9a3a472a96c9754bc9
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2022-02-10 20:05:47 +0000

    Added initial data manager files

---
+++ include/data.h
@@ -0,0 +1,8 @@
+//
+// Created by mattw on 10/02/2022.
+//
+
+#ifndef BECUDA_DATA_H
+#define BECUDA_DATA_H
+
+#endif //BECUDA_DATA_H
---
+++ src/data.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mattw on 10/02/2022.
+//
+
+#include "data.h"
