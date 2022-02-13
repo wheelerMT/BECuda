8ded3beba15081ec9cb8a3ba40db1ac7eaf38f90
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2022-02-13 20:00:19 +0000

    Added first spin-1 parameters struct

--- include/data.h
+++ include/data.h
@@ -5,4 +5,25 @@
 #ifndef BECUDA_DATA_H
 #define BECUDA_DATA_H
 
+#include "highfive/H5File.hpp"
+
+struct Spin1Parameters
+{
+    Spin1Parameters(double c0, double c2, double p, double q, int nt, double dt);
+
+    double spinIndependentInteraction;
+    double spinDependentInteraction;
+    double linearZeeman;
+    double quadraticZeeman;
+
+    int numOfTimeSteps;
+    double timeStep;
+};
+
+
+class DataManager
+{
+
+};
+
 #endif //BECUDA_DATA_H
--- src/data.cpp
+++ src/data.cpp
@@ -3,3 +3,9 @@
 //
 
 #include "data.h"
+
+Spin1Parameters::Spin1Parameters(double c0, double c2, double p, double q, int nt, double dt)
+        : spinIndependentInteraction{c0}, spinDependentInteraction{c2}, linearZeeman{p}, quadraticZeeman{q},
+        numOfTimeSteps{nt}, timeStep{dt}
+{
+}
