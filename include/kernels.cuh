f33072d6e71e382058db1f0443496be0cef909da
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2022-02-14 19:32:59 +0000

    Added empty kernels files

---
+++ include/kernels.cuh
@@ -0,0 +1,8 @@
+//
+// Created by mattw on 14/02/2022.
+//
+
+#ifndef BECUDA_KERNELS_CUH
+#define BECUDA_KERNELS_CUH
+
+#endif //BECUDA_KERNELS_CUH
---
+++ src/kernels.cu
@@ -0,0 +1,5 @@
+//
+// Created by mattw on 14/02/2022.
+//
+
+#include "../include/kernels.cuh"
